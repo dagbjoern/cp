0837df673aecd18c6f1823720c445296e73eeb98
Lars Funke <lars.funke@udo.edu>
2018-04-18 15:29:14 +0200

    few tweaks

--- 0/2.cpp
+++ 0/2.cpp
@@ -1,40 +1,45 @@
 #include <iostream>
 #include <cmath>
 
+double relative_error(double x, double y)
+{
+  return abs(1 - x / y);
+}
+
 double a1(double x)
 {
-    return 1 / sqrt(x) - 1 / sqrt(x + 1);
+  return 1 / sqrt(x) - 1 / sqrt(x + 1);
 }
 
 double a2(double x)
 {
-    return 1 / (sqrt(x * (x + 1)) * (sqrt(x + 1) + sqrt(x)));
+  return 1 / (sqrt(x * (x + 1)) * (sqrt(x + 1) + sqrt(x)));
 }
 
 double b1(double x)
 {
-    return (1 - cos(x)) / sin(x);
+  return (1 - cos(x)) / sin(x);
 }
 
 double b2(double x)
 {
-    return tan(x / 2);
+  return tan(x / 2);
 }
 
 double c1(double x, double delta)
 {
-    return sin(x + delta) - sin(x);
+  return sin(x + delta) - sin(x);
 }
 
 double c2(double x, double delta)
 {
-    return -2 * sin(x) * pow(sin(delta / x), 2) + cos(x) * sin(delta);
+  return -2 * sin(x) * pow(sin(delta / x), 2) + cos(x) * sin(delta);
 }
 
 int main()
 {
-    std::cout << a1(1e25)  - a2(1e25)  << std::endl;
-    std::cout << b1(1e-25) - b2(1e-25) << std::endl;
-    std::cout << c1(1, 1e-25) - c2(1, 1e-25) << std::endl;
-    return 0;
+  std::cout << relative_error(a1(1e25), a2(1e25))  << std::endl;
+  std::cout << relative_error(b1(1e-25), b2(1e-25)) << std::endl;
+  std::cout << relative_error(c1(1, 1e-25), c2(1, 1e-25)) << std::endl;
+  return 0;
 }
--- 1/1.cpp
+++ 1/1.cpp
@@ -6,27 +6,34 @@
 #include <vector>
 #include <numeric>
 
-double saw2d(int steps, std::mt19937& rng)
+int saw2d(int steps, std::mt19937& rng)
 {
+	//create integer distribution to get int from [0,3]
 	std::uniform_int_distribution<> dist(0, 3);
+	//create boolean grid big enough for step count
 	bool lattice[2 * steps + 1][2 * steps + 1];
+	//initialize with false (= not visited)
 	memset(lattice, false, sizeof(lattice));
-	int x = steps - 1; int y = steps - 1;
+	//set origin at center of lattice
+	int x = steps;
+	int y = steps;
 
+	//iterate SAW steps
 	for(int i = 0; i < steps; i++)
 	{
 		int xnew, ynew;
 
-		//terminate loop if no further steps are possible
+		//terminate loop if no further steps are legal
 		if(lattice[x+1][y] && lattice[x-1][y] && lattice[x][y+1] && lattice[x][y-1])
 			break;
 
 		//bad implementation, worst case time complexity is infinite
-		//todo: remove impossible steps from random pool
+		//maybe remove illegal steps from random pool
 		do
 		{
 			xnew = x;
 			ynew = y;
+			//generate random number and choose step direction
 			switch(dist(rng))
 			{
 				case 0:	xnew = x + 1; break;
@@ -35,30 +42,36 @@ double saw2d(int steps, std::mt19937& rng)
 				case 3:	ynew = y - 1; break;
 			}
 		}
+		//repeat when illegal
 		while(lattice[xnew][ynew]);
 
+		//new legal step found -> go and save
 		x = xnew;
 		y = ynew;
 		lattice[x][y] = true;
 	}
-	//             (origin   )               (origin   )
-	return pow(x - (steps - 1), 2) + pow(y - (steps - 1), 2);
+	//compute squared distance from origin
+	return pow(x - steps, 2) + pow(y - steps, 2);
 }
 
 int main()
 {
+	//initialize PRNG
 	std::mt19937 rng;
 	rng.seed(std::random_device()());
-	int size = 1e5;
-	std::vector<int> output(size);
-	std::ofstream f;
-  f.open ("build/output.txt");
+	int size = 1e5; //sample size
+	int output[size]; //temporary output array
+	std::ofstream f;             //file to save results to
+	f.open ("build/output.txt"); //file to save results to
+	//iterate step counts
 	for(int j = 10; j <= 60; j += 5)
 	{
 		std::cout << j << "... " << std::flush;
+		//iterate samples and save to temp array
 		for(int i = 0; i < size; i++)
 			output[i] = saw2d(j, rng);
-		f << j << " " << std::accumulate(output.begin(), output.end(), 0) / (double) size << std::endl;
+		//compute average R_N^2 and save to file
+		f << j << " " << std::accumulate(output, output + size, 0) / (double) size << std::endl;
 	}
 	std::cout << std::endl;
 	return EXIT_SUCCESS;
